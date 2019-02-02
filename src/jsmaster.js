3d4d9ce7691be3ce27e1494982734fac445acca5
zhaozhiming <zhaozhiming@tik.com>
2019-02-02 23:37:55 +0800

    feat: use twitter api to fetch data

--- src/jsmaster.js
+++ src/jsmaster.js
@@ -1,54 +1,49 @@
-const puppeteer = require('puppeteer');
 const axios = require('axios');
+const jsdom = require('jsdom');
 
-const DINGDING_ROBOT =
-  'https://oapi.dingtalk.com/robot/send?access_token=2f5119f69f5437297e555c58e141f5d9441935cc5574586287a985664011a7b3';
+const { JSDOM } = jsdom;
+
+const getUrl = maxPos => {
+  const maxQuery = maxPos > 0 ? `&max_position=${maxPos}` : '';
+  return `https://twitter.com/kingzzm/lists/js-master/timeline?include_available_features=1&include_entities=1${maxQuery}&reset_error_state=false`;
+};
+
+const getMyWant = document => {
+  const tws = document.querySelectorAll(
+    'li.js-stream-item.stream-item.stream-item'
+  );
+  for (const tw of tws) {
+    const retwElem = tw.querySelector('.tweet-context.with-icn span a');
+    const isRetw = retwElem ? true : false;
+    // console.log({ isRetw });
+
+    const timeElem = tw.querySelector('.stream-item-header small a span');
+    const time = +timeElem.getAttribute('data-time-ms');
+    console.log({ time, date: new Date(time) });
+    // if (!isRetw && time < Date.now() - 24 * 60 * 60 * 1000) break;
+
+    const retwCountElem = tw.querySelector(
+      '.stream-item-footer .ProfileTweet-actionButton.js-actionButton.js-actionRetweet .ProfileTweet-actionCountForPresentation'
+    );
+    const retwCount = +retwCountElem.textContent;
+    // console.log({ retwCount });
+    // if (retwCount < 30) continue;
+
+    const infoElem = tw.querySelector(
+      '.tweet.js-stream-tweet.js-actionable-tweet.js-profile-popup-actionable'
+    );
+    const id = infoElem.getAttribute('data-tweet-id');
+    const link = infoElem.getAttribute('data-permalink-path');
+    const url = `https://twitter.com${link}`;
+    console.log({ id });
+  }
+};
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://twitter.com/kingzzm/lists/js-master', {
-    timeout: 10 * 1000,
-  });
-  const tws = await page.$$('li.js-stream-item.stream-item.stream-item');
-  for (const tw of tws) {
-    let isRetw = false;
-    try {
-      await tw.$eval('.tweet-context.with-icn span a', e =>
-        e.getAttribute('data-user-id')
-      );
-      isRetw = true;
-    } catch (e) {
-      isRetw = false;
-    }
-    console.log({ isRetw });
-
-    const time = await tw.$eval(
-      '.stream-item-header small a span',
-      e => +e.getAttribute('data-time-ms')
-    );
-    console.log({ time, date: new Date(time) });
-    if (!isRetw && time < Date.now() - 24 * 60 * 60 * 1000) break;
-
-    const retwCount = await tw.$eval(
-      '.stream-item-footer .ProfileTweet-actionButton.js-actionButton.js-actionRetweet .ProfileTweet-actionCountForPresentation',
-      e => +e.innerText
-    );
-    console.log({ retwCount });
-    if (retwCount < 30) continue;
-
-    const info = await tw.$eval(
-      '.tweet.js-stream-tweet.js-actionable-tweet.js-profile-popup-actionable',
-      e => e.getAttribute('data-permalink-path')
-    );
-    console.log({ info });
-    const url = `https://twitter.com${info.link}`;
-    //   await axios.post(DINGDING_ROBOT, {
-    //     msgtype: 'text',
-    //     text: {
-    //       content: url,
-    //     },
-    //   });
-  }
-  await browser.close();
+	const max = 0
+  const resp = await axios.get(getUrl(max));
+  const { data } = resp;
+  console.log({ min: data['min_position'], max });
+  const dom = new JSDOM(data['items_html']);
+  getMyWant(dom.window.document);
 })();
